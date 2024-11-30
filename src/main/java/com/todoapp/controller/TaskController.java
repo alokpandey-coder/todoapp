ba440e09248a6bf61de21c46ffb5feb25cdd6aaa
Your Name <your_email@example.com>
2024-11-30 14:46:09 +0530

    Completeed Project

--- src/main/java/com/todoapp/controller/TaskController.java
+++ src/main/java/com/todoapp/controller/TaskController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.todoapp.module.Task;
 import com.todoapp.service.TaskService;
@@ -27,4 +30,26 @@ public class TaskController {
         model.addAttribute("tasks", tasks);
         return "tasks";
     }
+
+    @PostMapping
+    public String createTasks(@RequestParam String title){
+
+        taskService.createTasks(title);
+
+        return "redirect:/tasks";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+       taskService.deleteTask(id);
+        return "redirect:/tasks";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id){
+       taskService.toggleTask(id);
+        return "redirect:/tasks";
+    }
+
+
 }
--- src/main/java/com/todoapp/service/TaskService.java
+++ src/main/java/com/todoapp/service/TaskService.java
@@ -23,5 +23,29 @@ public class TaskService {
     }
 
 
+    public void createTasks(String title) {   
+      Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+       
+      taskRepository.deleteById(id);
+    }
+
+    public void toggleTask(Long id) {
+      
+      Task task = taskRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid Tasks"));
+
+      task.setCompleted(!task.isCompleted());
+
+      taskRepository.save(task);
+    }
+
+    
+
+
 
 }
